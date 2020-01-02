49720a787d614cde6480040d19a228a80b9a33f4
itays <shklari@post.bgu.ac.il>
2020-01-02 15:06:44 +0200

    Data interfaces

---
+++ Domain/Mail/MailHandler.js
@@ -0,0 +1,14 @@
+class Mail_Handler {
+    handle() {
+    };
+}
+
+class Costumer_Mail_Handler extends Mail_Handler {
+    handle() {
+    };
+}
+
+class Lawyer_Mail_Handler extends Mail_Handler {
+    handle(self) {
+    };
+}
