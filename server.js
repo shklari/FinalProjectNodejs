f0bfad62fb6bb06f69d6ca7630f26f220ed43150
itays <shklari@post.bgu.ac.il>
2020-01-02 15:55:27 +0200

    Data interfaces

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const apartments = [
+const port = 8080;
+Lawyers = [
+    {"name": "aa", "password": "11", "email": "aa@gmail.com"},
+    {"name": "bb", "password": "11", "email": "bb@gmail.com"},
+    {"name": "cc", "password": "22", "email": "cc@gmail.com"}
+];
+
+Costumers = [
+    {"name": "aa1", "password": "11", "email": "aa1@gmail.com"},
+    {"name": "bb1", "password": "11", "email": "bb1@gmail.com"},
+    {"name": "cc1", "password": "22", "email": "cc1@gmail.com"}
+];
+
+const properties = [
     {block: 1, building: 2, apartment: 1},
     {block: 1, building: 1, apartment: 1},
     {block: 1, building: 1, apartment: 2},
@@ -17,7 +29,7 @@ app.get('/apartments/:block?/:building?/:apartment?', (req, res) => {
     const block = req.params.block;
     const building = req.params.building;
     const apartment = req.params.apartment;
-    let filteredProperties = apartments;
+    let filteredProperties = properties;
     console.log(`Block: ${block}, building: ${building}, apartment: ${apartment}`);
 
     if (block) {
@@ -35,5 +47,7 @@ app.get('/apartments/:block?/:building?/:apartment?', (req, res) => {
 
     res.send(filteredProperties);
 });
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
